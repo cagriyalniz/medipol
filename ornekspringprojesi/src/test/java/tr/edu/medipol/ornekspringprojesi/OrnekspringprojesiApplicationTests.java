ef1a52a9e35bb04bc72e07fc286272be1d79b7a8
bprlab <bprlab@DESKTOP-I75BABB>
2018-10-13 14:28:08 +0300

    YENİ BİR SPRİNG WEB, ACTUATOR PROJESİ

---
+++ ornekspringprojesi/src/test/java/tr/edu/medipol/ornekspringprojesi/OrnekspringprojesiApplicationTests.java
@@ -0,0 +1,16 @@
+package tr.edu.medipol.ornekspringprojesi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrnekspringprojesiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
